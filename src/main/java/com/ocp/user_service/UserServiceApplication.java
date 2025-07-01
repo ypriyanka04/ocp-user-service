aa86ad6196ba76454870d41cabd2dff2cc4030e1
priyanka <priyanka.decipherzone@gmail.com>
2025-07-01 17:37:45 +0530

    added basic configurations

--- src/main/java/com/ocp/user_service/UserServiceApplication.java
+++ src/main/java/com/ocp/user_service/UserServiceApplication.java
@@ -2,12 +2,22 @@ package com.ocp.user_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@EnableDiscoveryClient
 @SpringBootApplication
+@RestController
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserServiceApplication.class, args);
 	}
+
+	@GetMapping
+	public String getMessage(){
+        return "This is my user service";
+    }
 
 }
